73f86fc8a8959100229ddd79aa5fc811f8ae916d
Oli Davis <oli.davis@me.com>
2018-03-16 12:21:08 +0000

    bump fix version

--- pydnserver/_metadata.py
+++ pydnserver/_metadata.py
@@ -4,7 +4,7 @@
 __module_name__ = u'PyDNServer'
 __description__ = u'Provides a very simple DNS server.'
 
-__version__ = u'2.1.1'
+__version__ = u'2.1.2'
 
 __author__ = u'Oli Davis & Hywel Thomas'
 __authorshort__ = u'OWBD_HT'
