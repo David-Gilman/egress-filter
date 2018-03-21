6f597e989ecdb49b373ab9722f0c74c16465a675
Oli Davis <oli.davis@me.com>
2018-03-21 10:24:15 +0000

    add some os/user detection to adhoc so we dont have to keep tweaking port for testing

--- adhoc.py
+++ adhoc.py
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+import os
+import sys
 import logging_helper
 from configurationutil import Configuration, cfg_params
 from pydnserver import DNSServerRootWindow, __version__, __authorshort__, __module_name__
@@ -13,8 +15,10 @@ cfg_params.APP_AUTHOR = __authorshort__
 cfg_params.APP_VERSION = __version__
 Configuration()  # Force config load!
 
-logging = logging_helper.setup_logging(level=logging_helper.DEBUG,
+logging = logging_helper.setup_logging(level=logging_helper.INFO,
                                        log_to_file=False)
+
+dns_port = 8053 if sys.platform == u'darwin' and os.geteuid() != 0 else 53
 
 # create a domain list where some items have friendly names and some don't!
 address_list = [(host_cfg.domain, host_name) for host_name, host_cfg in iteritems(Hosts())]
@@ -22,5 +26,5 @@ address_list.insert(1, u'google.co.uk')
 address_list.append(u'bbc.co.uk')
 address_list.append((u'sky.com', u''))
 
-dns = DNSServerRootWindow(server_kwargs=dict(port=9053),
+dns = DNSServerRootWindow(server_kwargs=dict(port=dns_port),
                           zone_address_list=address_list)
