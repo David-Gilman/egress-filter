3027566c61036038a793a3f1316c32a6b4fff6ef
Hywel Thomas <hywel.thomas@sky.uk>
2018-09-10 19:38:45 +0100

    Refactor to use RadioButton widget.

--- pydnserver/gui/frame/forwarders.py
+++ pydnserver/gui/frame/forwarders.py
@@ -1,9 +1,8 @@
 # encoding: utf-8
 
 import logging_helper
-from tkinter import StringVar
 from uiutil.tk_names import HORIZONTAL, W, NSEW, askquestion, showerror
-from uiutil import BaseFrame, Button, Separator, Label, Position
+from uiutil import BaseFrame, Button, Separator, Label, Position, RadioButton
 from configurationutil import Configuration
 from ...config import dns_forwarders
 from ..window.forwarder import AddEditForwarderWindow, AddEditForwarderFrame
@@ -11,9 +10,50 @@ from ..window.forwarder import AddEditForwarderWindow, AddEditForwarderFrame
 logging = logging_helper.setup_logging()
 
 
+class RecordFrame(BaseFrame):
+
+    def __init__(self,
+                 selected=None,
+                 *args,
+                 **kwargs):
+
+        BaseFrame.__init__(self,
+                           layout=HORIZONTAL,
+                           *args,
+                           **kwargs)
+
+        Label(text=u'Network',
+              sticky=W)
+
+        Label(text=u'Forwarders',
+              sticky=W)
+
+        Separator()
+
+        for interface, forwarders in iter(dns_forwarders.get_all_forwarders().items()):
+            self.radio_button = RadioButton(text=interface,
+                                            value=interface,
+                                            row=Position.NEXT,
+                                            sticky=W)
+            if selected == interface or selected is None:
+                selected = interface
+
+            Label(text=u', '.join(forwarders),
+                  sticky=W)
+
+        self.radio_button.value = selected
+
+        Separator()
+
+        self.nice_grid()
+
+    @property
+    def selected_record(self):
+       return self.radio_button.value
+
+
 class ForwarderConfigFrame(BaseFrame):
 
-    AUTO_POSITION = HORIZONTAL
     BUTTON_WIDTH = 15
 
     def __init__(self,
@@ -21,62 +61,33 @@ class ForwarderConfigFrame(BaseFrame):
                  **kwargs):
 
         BaseFrame.__init__(self,
+                           layout=HORIZONTAL,
                            *args,
                            **kwargs)
-
-        self._selected_record = StringVar(self.parent)
 
         self.cfg = Configuration()
 
         self.nameserver_radio_list = {}
 
-        self.record_frame_position = dict(row=self.row.current,
-                                          column=self.column.current,
-                                          sticky=NSEW)
+        self.record_frame_kwargs = dict(row=self.row.current,
+                                        column=self.column.current,
+                                        sticky=NSEW)
         self._add_buttons()
 
-        self.record_frame_position['columnspan'] = self.column.max
+        self.record_frame_kwargs['columnspan'] = self.column.max
 
         self._build_record_frame()
 
         self.nice_grid()
 
-    def _build_record_frame(self):
+    def _build_record_frame(self,
+                            selected=None):
+        self.record_frame = RecordFrame(selected=selected,
+                                        **self.record_frame_kwargs)
 
-        self.record_frame = BaseFrame(**self.record_frame_position)
-        self.record_frame.AUTO_POSITION = HORIZONTAL
-
-        Label(frame=self.record_frame,
-              text=u'Network',
-              sticky=W)
-
-        Label(frame=self.record_frame,
-              text=u'Forwarders',
-              sticky=W)
-
-        Separator(frame=self.record_frame)
-
-        select_next_row = True
-        for interface, forwarders in iter(dns_forwarders.get_all_forwarders().items()):
-
-            if select_next_row:
-                self._selected_record.set(interface)
-                select_next_row = False
-
-            self.nameserver_radio_list[interface] = self.radio_button(frame=self.record_frame,
-                                                                      text=interface,
-                                                                      variable=self._selected_record,
-                                                                      value=interface,
-                                                                      row=Position.NEXT,
-                                                                      sticky=W)
-
-            self.label(frame=self.record_frame,
-                       text=u', '.join(forwarders),
-                       sticky=W)
-
-        Separator(frame=self.record_frame)
-
-        self.record_frame.nice_grid()
+    @property
+    def selected_record(self):
+       return self.record_frame.selected_record
 
     def _add_buttons(self):
 
@@ -93,6 +104,13 @@ class ForwarderConfigFrame(BaseFrame):
                width=self.BUTTON_WIDTH,
                command=self._edit_forwarder)
 
+    def _refresh(self):
+        selected = self.selected_record
+        self.record_frame.destroy()
+        self._build_record_frame(selected=selected)
+        self.nice_grid()
+        self.parent.master.update_geometry()
+
     def _add_forwarder(self):
         window = AddEditForwarderWindow(fixed=True,
                                         parent_geometry=self.parent.winfo_toplevel().winfo_geometry())
@@ -100,30 +118,20 @@ class ForwarderConfigFrame(BaseFrame):
         window.transient()
         window.grab_set()
         self.parent.wait_window(window)
-
-        self.record_frame.destroy()
-        self._build_record_frame()
-        self.nice_grid()
-
-        self.parent.master.update_geometry()
+        self._refresh()
 
     def _edit_forwarder(self):
-        window = AddEditForwarderWindow(selected_record=self._selected_record.get(),
+        window = AddEditForwarderWindow(selected_record=self.selected_record,
                                         edit=True,
                                         fixed=True,
                                         parent_geometry=self.parent.winfo_toplevel().winfo_geometry())
         window.transient()
         window.grab_set()
         self.parent.wait_window(window)
-
-        self.record_frame.destroy()
-        self._build_record_frame()
-        self.nice_grid()
-
-        self.parent.master.update_geometry()
+        self._refresh()
 
     def _delete_forwarder(self):
-        selected = self._selected_record.get()
+        selected = self.selected_record
 
         if selected == AddEditForwarderFrame.DEFAULT_NETWORK:
             showerror(title=u'Default Forwarder',
@@ -138,11 +146,6 @@ class ForwarderConfigFrame(BaseFrame):
             if result == u'yes':
                 key = u'{cfg}.{int}'.format(cfg=dns_forwarders.DNS_SERVERS_CFG,
                                             int=selected)
-
                 del self.cfg[key]
 
-                self.record_frame.destroy()
-                self._build_record_frame()
-                self.nice_grid()
-
-                self.parent.master.update_geometry()
+                self._refresh()
